35efc358868c7506f7e1f17573ea8835e840f1d2
MoeX <54111871+MengXin001@users.noreply.github.com>
2023-08-13 15:28:35 +0800

    creaft: GF4 xml parser

---
+++ GF/GF4_XML.py
@@ -0,0 +1,12 @@
+import xml.dom.minidom
+def read_xml(filepath):
+    xmldata = {}
+    with open(filepath, "r", encoding="utf-8") as f:
+        xmlf = xml.dom.minidom.parse(f)
+        root = xmlf.documentElement
+        keys = ['SatelliteID', 'SensorID', 'CenterTime', 'ImageGSD', 'TopLeftLatitude', 'TopLeftLongitude',
+        'TopRightLatitude', 'TopRightLongitude', 'BottomRightLatitude', 'BottomRightLongitude',
+        'BottomLeftLatitude', 'BottomLeftLongitude']
+        for key in keys:
+            xmldata[key] = filename_node = root.getElementsByTagName(key)[0].childNodes[0].data
+        return xmldata
